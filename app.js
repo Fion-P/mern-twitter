212fae92037f8d165fd8336a1398c3ba709825ba
Fion <fionpang97@gmail.com>
2019-11-18 11:54:14 -0800

    finish merntwitter

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const tweets = require("./routes/api/tweets");
 const User = require('./models/User');
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true})
